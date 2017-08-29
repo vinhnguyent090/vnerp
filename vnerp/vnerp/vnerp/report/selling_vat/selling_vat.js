b52089722044018803c3956b5182ba8c35e6d358
Vinh <vinhnguyen.t090@gmail.com>
2017-08-29 23:20:19 +0700

    working VAT report

---
+++ vnerp/vnerp/vnerp/report/selling_vat/selling_vat.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2016, Vinhbk2000 and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Selling VAT"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": "From Date",
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date"),
+			"width": "80"
+		},
+		{
+			"fieldname":"to_date",
+			"label": "To Date",
+			"fieldtype": "Date",
+			"default": get_today()
+		},
+		{
+			"fieldname":"company",
+			"label": "Company",
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": sys_defaults.company
+		}
+	]
+}
